239feb92ae6d299421ea774747d1346dc643b16f
Pengfei Wang <pengfei.wang@gmail.com>
2026-06-14 15:07:58 +0800

    chore: release data-sdk v0.1.3

--- Sources/DataGatewayClient/FilePreparation.swift
+++ Sources/DataGatewayClient/FilePreparation.swift
@@ -13,7 +13,7 @@ public enum DataGatewayClientModule {
     public static let name = "DataGatewayClient"
 
     /// SDK version reported to the device initialization service.
-    public static let version = "0.1.2"
+    public static let version = "0.1.3"
 }
 
 /// Public configuration for device initialization and reinitialization.
